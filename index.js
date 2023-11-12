e28e335655f4b0e983770256060c7c89d1c72266
elayne <elacheu@gmail.com>
2023-11-12 13:12:42 -0600

    shape classes added, moved cli to index

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
-const CLI = require('./lib/cli.js');
+const inquirer = require('inquirer')
+const fs = require('fs')
+const { Triangle, Square, Circle } = require('./lib/shapes')
 
-const cli = new CLI();
+// prompt questions for svg traits //
+const questions = [
+    {
+        type: 'input',
+        name: 'characters',
+        message: 'Enter up to three characters for the logo text',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the color for the logo text (color keyword or hexadecimal number)',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        choices: ['circle', 'triangle', 'square'],
+        message: 'Choose the shape for the logo (circle, triangle, square)',
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the color for the shape (color keyword or hexadecimal number)',
+    },
+];
 
-cli.run();
+// generate logo based off of prompt inputs //
+function generateLogo(answer) {
+    const { characters, textColor, shape, shapeColor } = answers;
+}
+
+// initalize application //
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        generateLogo(answers)
+    });
+}
+
+init();
+
 
 
 // GIVEN a command-line application that accepts user input
